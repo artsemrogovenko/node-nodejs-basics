de6e25cf988baa6672e5a4d9f2be2474ff6a1011
artsemrogovenko <taftoos2@gmail.com>
2025-10-21 23:38:18 +0300

    feat: implement fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { writeFile, readFile } from "node:fs";
+
+const fileName = "src/fs/files/fresh.txt";
+const content = "I am fresh and young";
+const failedMessage = "FS operation failed";
+
 const create = async () => {
-  // Write your code here
+  try {
+    isNotExistFile();
+    writeFile(fileName, content, { signal }, (err) => {
+      if (err) console.log(err);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await create();
+
+async function isNotExistFile() {
+  readFile(fileName, (err, data) => {
+    if (data) throw new Error(failedMessage);
+  });
+}
