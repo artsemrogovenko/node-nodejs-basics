e2260c9fd7b7e06e032d772cc428fed8ac2f5b4e
artsemrogovenko <taftoos2@gmail.com>
2025-10-25 14:38:18 +0300

    feat: implement zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import {  createGzip } from "node:zlib";
+
 const compress = async () => {
-  // Write your code here
+  const src = "src/zip/files/fileToCompress.txt";
+  const target = "src/zip/archive.gz";
+  const reader = createReadStream(src,{autoClose:true});
+  const write = createWriteStream(target,{autoClose:true});
+  const gzip = createGzip( );
+
+reader.pipe(gzip).pipe(write)
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import {  createGunzip } from "node:zlib";
+
 const decompress = async () => {
-  // Write your code here
+  const src = "src/zip/archive.gz";
+  const target = "src/zip/fileToCompress.txt";
+  const reader = createReadStream(src,{autoClose:true});
+  const write = createWriteStream(target,{autoClose:true});
+  const unzip = createGunzip( );
+
+  reader.pipe(unzip).pipe(write)
 };
 
 await decompress();
